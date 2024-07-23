076c144be4441a4f30373f1ff4dc93a5ecc8ca4e
EdithTech <yash9atalkar@gmail.com>
2024-07-24 01:40:11 +0530

    removed auth file

--- auth.js
+++ auth.js
@@ -1,26 +1,26 @@
-import passport from "passport";
-import { Strategy } from 'passport-local';
-import userModel from "../To Dp List/model/user.js";
+// import passport from "passport";
+// import { Strategy } from 'passport-local';
+// import userModel from "../To Dp List/model/user.js";
 
-passport.use(new Strategy(async (username, passwort, done) => {
-    try{
-        console.log(username, passwort);
-        const user = await userModel.findOne({username: username});
+// passport.use(new Strategy(async (username, passwort, done) => {
+//     try{
+//         console.log(username, passwort);
+//         const user = await userModel.findOne({username: username});
 
-        if(!user){
-            console.log("Username is invalid");
-            return done(null, false, {message: "Username is invalid"});
-        }
+//         if(!user){
+//             console.log("Username is invalid");
+//             return done(null, false, {message: "Username is invalid"});
+//         }
 
-        const isPasswordMatched = user.comparePassword(passwort);
-        if(isPasswordMatched){
-            return done(null, user);
-        }else{
-            return done(null, false, {message: "Password is Incorrect"})
-        }
-    }catch(error){
-        console.log(error);
-    }
-}))
-
-export default passport;
+//         const isPasswordMatched = user.comparePassword(passwort);
+//         if(isPasswordMatched){
+//             return done(null, user);
+//         }else{
+//             return done(null, false, {message: "Password is Incorrect"})
+//         }
+//     }catch(error){
+//         console.log(error);
+//     }
+// }))
+// // 
+// export default passport;
